8fb0142c5d1ca3886eb6ecea470738ed5b4c2349
Jeremy DEVERDUN <jeremy.deverdun@neuf.fr>
2013-12-04 17:37:43 +0100

    ras

--- SEB/src/solver/EquationSolver.java
+++ SEB/src/solver/EquationSolver.java
@@ -9,43 +9,52 @@ import models.Variable;
 import Jama.Matrix;
 
 public class EquationSolver {
-    public static final double EPSILON = 1e-4;
+	public static final double EPSILON = 1e-4;
 
-    // Newton's method to find x* such that f(x*) = 0, starting at x
-    public static Matrix root(Architecture archi, ArrayList<Variable> variables) {
-    	Matrix x = null;
-    	int c=0;
-        while (true) {
-        	System.out.println(c++);
-        	ArrayList<Matrix> mats = archi.eval(variables);
-            Matrix f = mats.get(0);
-            Matrix J = mats.get(1);//f.jacobian(x);
-            x = mats.get(2);
-           /* for (int i = 0; i < J.getRowDimension(); i++) {
-                for (int j = 0; j < J.getColumnDimension(); j++) 
-                    System.out.printf("%9.4f ", J.get(i,j));
-                System.out.println();
-            }*/
-            Matrix delta = J.inverse().times(f);
-            x = x.minus(delta);
-            updateVariablesFromMatrix(variables,x);
-            if (delta.norm1() < EPSILON) break;
-        }
-        
-        return x;
-    }
+	// Newton's method to find x* such that f(x*) = 0, starting at x
+	public static Matrix root(Architecture archi, ArrayList<Variable> variables) {
+		Matrix x = null;
+		int c=0;
+		while (true) {
+			System.out.println(c++);
+			ArrayList<Matrix> mats = archi.eval(variables);
+			Matrix f = mats.get(0);
+			Matrix J = mats.get(1);//f.jacobian(x);
+			x = mats.get(2);
+			Matrix delta = J.inverse().times(f);
+			x = x.minus(delta);
+			updateVariablesFromMatrix(variables,x);
+			if (delta.norm1() < EPSILON) break;
+		}
 
-    /**
-     * On met a jours l'arraylist des variables a partir d'une matrix
-     * @param variables
-     * @param x
-     * @return
-     */
+		return x;
+	}
+
+	/**
+	 * On met a jours l'arraylist des variables a partir d'une matrix
+	 * @param variables
+	 * @param x
+	 * @return
+	 */
 	private static void updateVariablesFromMatrix(
 			ArrayList<Variable> variables, Matrix x) {
 		int m = x.getRowDimension();
-        for(int i = 0; i < m; i++){
-        	variables.get(i).setValue((float) x.get(i, 0));
-        }
+		for(int i = 0; i < m; i++){
+			variables.get(i).setValue((float) x.get(i, 0));
+		}
+	}
+
+	/**
+	 * Affiche a l'ecran la matrice x precede du message msg
+	 * @param x 
+	 * @param msg
+	 */
+	private static void show(Matrix x, String msg){
+		System.out.println(msg);
+		for (int i = 0; i < x.getRowDimension(); i++) {
+			for (int j = 0; j < x.getColumnDimension(); j++) 
+				System.out.printf("%9.4f ", x.get(i,j));
+			System.out.println();
+		}
 	}
 }
